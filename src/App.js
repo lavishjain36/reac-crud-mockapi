3c207f0954fbbd376651a3bcd77e5f1c3d27acf6
Lavish <lavishjain36@gmil.com>
2022-12-02 22:11:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CreateUser from "./pages/Create_page/CreateUser";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import Edit from "./pages/Edit_page/Edit";
-import Profile from "./pages/Profile_page/Profile";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/create" element={<CreateUser />} />
-        <Route path="/profile/:id" element={<Profile />} />
-      </Routes>
+    <Navbar/>
+    <Routes>
+      <Route path="/"  element={<Dashboard/>}/>
+      <Route path="/create" element={<CreateUser/>}/>
+    </Routes>
     </BrowserRouter>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,16 @@
-import React from "react";
-import { SiDiscord } from "react-icons/si";
-import { Link } from "react-router-dom";
-
-const Navbar = () => {
+import React from 'react'
+import {SiDiscord} from "react-icons/si"
+import { Link } from 'react-router-dom'
+const Navbar=()=> {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container px-4 px-lg-5">
-        <Link className="navbar-brand" to="/">
-          <SiDiscord className="mx-1" />
-          users
-        </Link>
-      </div>
+    <div className="container px-4 px-lg-5">
+     <Link className="navbar-brand" to="/">
+        <SiDiscord className="mx-1"/>
+        users
+     </Link>
+    </div>
     </nav>
-  );
-};
-
-export default Navbar;
+  )
+}
+export default Navbar
